62798065b10f69ad2f4fa2dd4933d7481caa5b4e
klayton Junior <klayton.junior@iaris.com>
2025-03-17 13:05:22 -0300

    feat: using the easyinspection mask component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "easyinspection-mask";
 
-function App() {
+const App = () => {
+  const [mask, setMask] = useState("car-sedan");
+  const videoRef = useRef(null);
+  const [stream, setStream] = useState(null);
+
+  useEffect(() => {
+    const startCamera = async () => {
+      try {
+        const userStream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        videoRef.current.srcObject = userStream;
+        setStream(userStream);
+      } catch (err) {
+        console.error("Erro ao acessar a câmera:", err);
+      }
+    };
+
+    startCamera();
+
+    return () => {
+      if (stream) {
+        stream.getTracks().forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>EasyInspection Mask Web Component</h1>
+      <select onChange={(e) => setMask(e.target.value)} value={mask}>
+        <option value="none">Sem máscara</option>
+        <option value="chassi-motor">chassi-motor</option>
+        <option value="odometer">odometer</option>
+        <option value="small-vehicle-front">small-vehicle-front</option>
+        <option value="small-vehicle-side-left">small-vehicle-side-left</option>
+        <option value="small-vehicle-side-right">
+          small-vehicle-side-right
+        </option>
+        <option value="motocycle-front">motocycle-front</option>
+        <option value="motocycle-side-left">motocycle-side-left</option>
+        <option value="motocycle-side-right">motocycle-side-right</option>
+        <option value="big-vehicle-front">big-vehicle-front</option>
+        <option value="bus-side-left">bus-side-left</option>
+        <option value="bus-side-right">bus-side-right</option>
+        <option value="truck-side-left">truck-side-left</option>
+        <option value="truck-side-right">truck-side-right</option>
+      </select>
+
+      <div
+        style={{
+          position: "relative",
+          width: "600px",
+          height: "300px",
+          margin: "auto",
+          marginTop: "30px",
+        }}
+      >
+        {/* Máscara do Web Component */}
+        <svg-mask maskName={mask} width="90%">
+          {/* Câmera */}
+          <video
+            ref={videoRef}
+            autoPlay
+            playsInline
+            style={{ width: "100%", height: "100%", borderRadius: "15px" }}
+          />
+        </svg-mask>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
